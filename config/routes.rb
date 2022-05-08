b25cc4d995d68e39bcce7a1694b387144cadeb53
Eddie <eddie@5xcampus.com>
2022-05-08 13:42:44 +0800

    add model product

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/products/new", to: "products#new", as: "new_product"
+
+  root "pages#home"
 end
