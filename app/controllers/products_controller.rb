dd6f3bd138f0acd9e86a6d41993ece3efb990877
Eddie <eddie@5xcampus.com>
2022-05-08 17:04:41 +0800

    Create and Read and Destroy record

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,37 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params["id"])
+  end
+
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product = Product.find(params["id"])
+    @product.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :title, :price, :sku, :description
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "/products/new", to: "products#new", as: "new_product"
+  get "/products", to: "products#index"
+  post "/products", to: "products#create"
+  get "/products/:id", to: "products#show", as: "product"
+  delete "/products/:id", to: "products#destroy"
 
-  root "pages#home"
+  root "products#index"
 end
